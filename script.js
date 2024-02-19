86368ea961adbdb94e1ae68e8d7c62b2495a0787
MiguelBeltran-bit <84816641+MiguelBeltran-bit@users.noreply.github.com>
2024-02-18 19:08:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Cambiar el color de fondo del encabezado al hacer clic en él
+    $('header').click(function() {
+      var randomColor = '#' + Math.floor(Math.random()*16777215).toString(16); // Generamos un color aleatorio en formato hexadecimal
+      $(this).css('background-color', randomColor); // Cambiamos el color de fondo del encabezado
+    });
+  });
+  
